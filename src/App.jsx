9e6ea63515c61ff643602e4ef37d3fe5952fd009
isabellexvr <isabellexavier59@gmail.com>
2023-07-23 20:16:29 -0300

    style: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,94 @@
 import styled from "styled-components";
 import * as Scroll from "react-scroll";
 import { useRef } from "react";
+import Header from "./components/Header";
 
 function App() {
   let Link = Scroll.Link;
   let Element = Scroll.Element;
-  let scrollSpy = Scroll.scrollSpy
+  let scrollSpy = Scroll.scrollSpy;
 
-  console.log(scrollSpy)
+  console.log(scrollSpy);
+
+  var observer = new IntersectionObserver(
+    function (entries) {
+      // isIntersecting is true when element and viewport are overlapping
+      // isIntersecting is false when element and viewport don't overlap
+      if (entries[0].isIntersecting === true) {
+        console.log("Element has just become visible in screen");
+      }
+    },
+    { threshold: [0] }
+  );
+
+  let element = document.getElementById("1");
+
+  if (element) {
+    observer.observe(element);
+  }
 
   return (
-    <>
+    <AppContainer>
+      <Header />
+      <LightElement>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1180"
+          height="90%"
+          viewBox="0 0 1180 1077"
+          fill="none"
+        >
+          <path
+            d="M610.5 -6.53579e-06L1179.5 -2.90379e-06L657.5 1077L0.500077 1077L610.5 -6.53579e-06Z"
+            fill="#D9D9D9"
+            fill-opacity="0.12"
+          />
+        </svg>
+      </LightElement>
+
       <Element name="first">
-        <div style={{width: "100px", height: "100vh", color: "blue"}} >first</div>
+        <div id="1" style={{ width: "100px", height: "100vh", color: "blue" }}>
+          first
+        </div>
       </Element>
       <Element name="second">
-        <div style={{width: "100px", height: "100vh", color: "blue"}} >sec</div>
+        <div id="2" style={{ width: "100px", height: "100vh", color: "blue" }}>
+          iii
+        </div>
       </Element>
       <Element name="3">
-        <div style={{width: "100px", height: "100vh", color: "blue"}} >3</div>
+        <div id="3" style={{ width: "100px", height: "100vh", color: "blue" }}>
+          3
+        </div>
       </Element>
       <Element name="4">
-        <div style={{width: "100px", height: "100vh", color: "blue"}} >4</div>
+        <div id="4" style={{ width: "100px", height: "100vh", color: "blue" }}>
+          4
+        </div>
       </Element>
-      <Link hashSpy={true} to="first">BORA</Link>
-    </>
+      <Link hashSpy={true} to="first">
+        BORA
+      </Link>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  height: fit-content;
+  width: 100%;
+  background-color: #161c26;
+  background: radial-gradient(
+      94.2% 35.03% at 54.82% 43.66%,
+      rgba(255, 255, 255, 0.08) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #161c26;
+`;
+
+const LightElement = styled.div`
+  position: absolute;
+  top: 0;
+  z-index: 0;
+`;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,127 @@
+import styled from "styled-components";
+import {
+  FaRegLightbulb,
+  FaFacebookF,
+  FaTwitter,
+  FaYoutube,
+  FaPhone,
+} from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { TfiMenu } from "react-icons/tfi";
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Logo>
+        <FaRegLightbulb />
+        <h1>
+          <strong>GR</strong>ILU
+        </h1>
+      </Logo>
+      <div className="contact">
+        <ContactIconHolder>
+          <FaFacebookF />
+        </ContactIconHolder>
+        <ContactIconHolder>
+          <FaTwitter />
+        </ContactIconHolder>
+        <ContactIconHolder>
+          <MdEmail />
+        </ContactIconHolder>
+        <ContactIconHolder>
+          <FaYoutube />
+        </ContactIconHolder>
+        <ContactIconHolder>
+          <FaPhone />
+        </ContactIconHolder>
+      </div>
+      <div className="menu">
+        <h1>MENU</h1>
+        <IconHolder>
+          <TfiMenu />
+        </IconHolder>
+      </div>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.div`
+  height: 80px;
+  width: 100%;
+  //background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 1;
+  .contact {
+    display: flex;
+    justify-content: space-evenly;
+    width: 23%;
+    //background-color: green;
+  }
+  .menu {
+    margin-right: 30px;
+    display: flex;
+    align-items: center;
+    font-family: 'Montserrat', sans-serif;
+    >h1{
+        color: white;
+        margin-right: 10px;
+        font-weight: 500;
+    }
+  }
+`;
+
+const Logo = styled.div`
+  color: #fff;
+  font-family: Tsukimi Rounded;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  width: 23%;
+  > svg {
+    font-size: 30px;
+  }
+  > h1 {
+    > strong {
+      color: #ae8034;
+    }
+  }
+`;
+
+const ContactIconHolder = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: #161c26;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  > svg {
+    color: white;
+    font-size: 25px;
+    opacity: 0.5;
+  }
+`;
+
+const IconHolder = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: rgb(217, 217, 217, 0.23);
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  > svg {
+    color: white;
+    font-size: 25px;
+  }
+`;
